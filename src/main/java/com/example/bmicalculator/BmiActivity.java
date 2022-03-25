36901c25bdfd941e07485796c2a47f4462499c1e
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2022-03-25 11:50:14 +0600

    UI Design for BMI

--- src/main/java/com/example/bmicalculator/BmiActivity.java
+++ src/main/java/com/example/bmicalculator/BmiActivity.java
@@ -2,14 +2,28 @@ package com.example.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BmiActivity extends AppCompatActivity {
 
+    android.widget.Button recalculateBMI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+        getSupportActionBar().hide();
+        recalculateBMI = findViewById(R.id.recalculateBmi);
+
+        recalculateBMI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BmiActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- src/main/java/com/example/bmicalculator/MainActivity.java
+++ src/main/java/com/example/bmicalculator/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    android.widget.Button calculateBMI;
+
+    TextView currentHeight;
+    TextView currentAge, currentWeight;
+    ImageView incrementAge, incrementWeight, decrementWeight, decrementAge;
+    SeekBar seekbarForHeight;
+    RelativeLayout Male, Female;
+
+    int weight = 50;
+    int age = 25;
+    int currentProgress;
+    String progress = "170";
+    String typeOfUser = "0";
+    String weight2 = "50";
+    String age2 = "25";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        calculateBMI = findViewById(R.id.calculateBmi);
+        currentAge = findViewById(R.id.currentage);
+        currentWeight = findViewById(R.id.currentweight);
+        currentHeight = findViewById(R.id.currentheight);
+        incrementAge = findViewById(R.id.incrementage);
+        incrementWeight = findViewById(R.id.incrementweight);
+        decrementWeight = findViewById(R.id.decrementweight);
+        decrementAge = findViewById(R.id.decrementweight);
+        seekbarForHeight = findViewById(R.id.seekbarforheight);
+        Male = findViewById(R.id.male);
+        Female = findViewById(R.id.female);
+
+
+
+
+        calculateBMI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BmiActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
